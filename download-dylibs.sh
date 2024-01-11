effad1fff1a1b53b7eabca04674254b87eb20dc9
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2024-01-12 07:46:03 +0900

    refact

--- download-dylibs.sh
+++ download-dylibs.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-LWJGL_VERSION=3.3.2
+LWJGL_VERSION="3.3.2"
 OS=macosx
 ARCH=arm64
 PREF=https://build.lwjgl.org/release/$LWJGL_VERSION/$OS/$ARCH
-wget $PREF/libbgfx.dylib
-wget $PREF/libglfw.dylib
-wget $PREF/liblwjgl.dylib
+
+# define dl function
+dl() {
+    if [ -f $1 ]; then
+        echo "$1 already exists"
+        return
+    fi
+    wget $PREF/$1
+}
+
+cd $(dirname $0)
+
+dl libbgfx.dylib
+dl libglfw.dylib
+dl liblwjgl.dylib
