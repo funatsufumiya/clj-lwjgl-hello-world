2fed72ce10818efe3fea0a1ee22bc9f53b8c2a1a
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2024-01-11 22:43:41 +0900

    remove license

--- project.clj
+++ project.clj
@@ -47,8 +47,6 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :main app.core
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies ~all-dependencies
   :repl-options {:init-ns app.core}
   :jvm-opts ^:replace ~(jvm-opts)
