573273c0d691bd7665abdd4b7f90c3af8896bd84
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2024-01-11 22:42:16 +0900

    refact

--- download-dylibs.sh
+++ download-dylibs.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-PREF=https://build.lwjgl.org/release/3.3.2/macosx/arm64
+LWJGL_VERSION=3.3.2
+OS=macosx
+ARCH=arm64
+PREF=https://build.lwjgl.org/release/$LWJGL_VERSION/$OS/$ARCH
 wget $PREF/libbgfx.dylib
 wget $PREF/libglfw.dylib
 wget $PREF/liblwjgl.dylib
--- project.clj
+++ project.clj
@@ -12,22 +12,44 @@
        (vec (set (concat (get JVM-OPTS :common)
                          (get JVM-OPTS os))))))
 
+(def LWJGL_PLATFORMS ["linux" "macos" "windows"])
+
+(def LWJGL_NS "org.lwjgl")
+
+(def LWJGL_VERSION "3.3.2")
+
+(def LWJGL_MODULES ["lwjgl"
+                    "lwjgl-bgfx"
+                    "lwjgl-glfw"
+                    ])
+
+(def no-natives? #{"lwjgl-egl" "lwjgl-jawt" "lwjgl-odbc"
+                   "lwjgl-opencl" "lwjgl-vulkan"})
+
+(defn lwjgl-deps-with-natives []
+  (apply concat
+         (for [m LWJGL_MODULES]
+           (let [prefix [(symbol LWJGL_NS m) LWJGL_VERSION]]
+             (into [prefix]
+                   (if (no-natives? m)
+                     []
+                     (for [p LWJGL_PLATFORMS]
+                       (into prefix [:classifier (str "natives-" p)
+                                     :native-prefix ""]))))))))
+
+(def all-dependencies
+  (into ;; Add your non-LWJGL dependencies here
+   '[[org.clojure/clojure "1.11.1"]
+     ]
+   (lwjgl-deps-with-natives)))
+
 (defproject clj-lwjgl-hello-world "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :main app.core
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [
-      [org.clojure/clojure "1.11.1"]
-      [org.lwjgl/lwjgl "3.3.2"]
-      [org.lwjgl/lwjgl "3.3.2" :classifier "natives-macos"]
-      [org.lwjgl/lwjgl-bgfx "3.3.2"]
-      ;; [org.lwjgl/lwjgl-opengl "3.3.2"]
-      ;; [org.lwjgl/lwjgl-opengl "3.3.2" :classifier "natives-macos"]
-      [org.lwjgl/lwjgl-glfw "3.3.2"]
-      [org.lwjgl/lwjgl-glfw "3.3.2" :classifier "natives-macos"]
-    ]
+  :dependencies ~all-dependencies
   :repl-options {:init-ns app.core}
   :jvm-opts ^:replace ~(jvm-opts)
   )
